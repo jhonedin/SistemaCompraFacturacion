3811f8076700bd8623699481f87ad7e553ace4f9
jhonedin <jhonedin1888@hotmail.com>
2021-09-08 22:38:34 -0500

    Modificacion en el settings configuracion base de datos postgresql

--- sistemacomprafacturacion/settings.py
+++ sistemacomprafacturacion/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sistemacomprafacturacion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sistema_cf',
+        'USER': 'postgres',
+        'PASSWORD': 'jhon',
+        'HOST': 'LOCALHOST',
+        'PORT': 5432
     }
 }
 
